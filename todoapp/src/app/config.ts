559a327728e12aeaa246b632acd5cb269522b657
TINTORRI Floren <floren.tintorri@etu.univ-amu.fr>
2024-03-22 22:44:33 +0100

    Confguration cypress et addaptation base de données

--- todoapp/src/app/config.ts
+++ todoapp/src/app/config.ts
@@ -2,11 +2,11 @@
 // ! A NE PAS PUSH lors de véritable projet > .gitignore
 
 export const configCustomer = {
-    supabaseUrl: "https://rjztxrmgksaiqczizfvz.supabase.co/rest/v1/customer",
+    supabaseUrl: "https://rjztxrmgksaiqczizfvz.supabase.co/rest/v1/customers",
     supabaseApiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqenR4cm1na3NhaXFjeml6ZnZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgzNTQzNTEsImV4cCI6MjAyMzkzMDM1MX0.73y6U15n4TfbctZwOvJ6PPFEoFHwDeG6whD_SOmgZJA"
   };
 
   export const configInvoice = {
-    supabaseUrl: "https://rjztxrmgksaiqczizfvz.supabase.co/rest/v1/invoice",
+    supabaseUrl: "https://rjztxrmgksaiqczizfvz.supabase.co/rest/v1/invoices",
     supabaseApiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqenR4cm1na3NhaXFjeml6ZnZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgzNTQzNTEsImV4cCI6MjAyMzkzMDM1MX0.73y6U15n4TfbctZwOvJ6PPFEoFHwDeG6whD_SOmgZJA"
   };
